60ee567e6155782f2e31b1283b8d17a5ff6b17c7
Lucas Holder <lucas.j.holder@gmail.com>
2025-03-12 15:44:31 -0700

    Added draw_move function.

--- drawables.py
+++ drawables.py
@@ -11,3 +11,44 @@ class Line():
 
   def draw(self, canvas:Canvas, fill_color):
     canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+class Cell():
+  def __init__(self, x1, x2, y1, y2, window):
+    self.left_wall = True
+    self.top_wall = True
+    self.right_wall = True
+    self.bottom_wall = True
+    self._x1 = x1
+    self._x2 = x2
+    self._y1 = y1
+    self._y2 = y2
+    self.win = window
+  
+  def draw(self, draw_color="#000000"):
+    point_top_left = Point(self._x1, self._y1)
+    point_bottom_left = Point(self._x1, self._y2)
+    point_top_right = Point(self._x2, self._y1)
+    point_bottom_right = Point(self._x2, self._y2)
+
+
+
+    if self.top_wall:
+      self.win.draw_line(Line(point_top_left, point_top_right), draw_color)
+    if self.left_wall:
+      self.win.draw_line(Line(point_top_left, point_bottom_left),draw_color)
+    if self.right_wall:
+      self.win.draw_line(Line(point_top_right, point_bottom_right),draw_color)
+    if self.bottom_wall:
+      self.win.draw_line(Line(point_bottom_left, point_bottom_right), draw_color)
+    return self
+  
+  def draw_move(self, to_cell, undo=False):
+
+    def get_middle(cell):
+      return Point(((cell._x1 + cell._x2) / 2), ((cell._y1 + cell._y2) / 2))
+    
+    point1 = get_middle(self)
+    point2 = get_middle(to_cell)
+    color = "gray" if undo else "red"
+    line = Line(point1, point2)
+    self.win.draw_line(line, color)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from window import Window
-from drawables import Point, Line
+from drawables import Point, Line, Cell
 win = Window(800, 600)
 point1 = Point(10,10)
 point2 = Point(100,100)
 line = Line(point1, point2)
 win.draw_line(line, "#FF007F")
+
+cell1 = Cell(10, 20, 10, 20, win).draw()
+cell2 = Cell(100, 300, 100, 300, win).draw("#FF007F")
+cell3 = Cell(500, 600, 200, 500, win).draw("#00FF00")
+
+cell2.draw_move(cell3)
+
 win.wait_for_close()
